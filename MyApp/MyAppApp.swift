c59c3c8c6c9fe6dd4741ecf10e9e9036e9c80576
jawahralharbi <ijawaher.harbi@gmail.com>
2024-01-24 17:28:21 +0300

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by Jawaher Alharbi on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
